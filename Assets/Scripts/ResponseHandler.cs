0947467644dc246d5713dd5ba8fdeb1cb84e5857
BrNm1 <Burit.noim@bumail.net>
2024-11-25 23:02:54 +0700

    res

--- Assets/Scripts/ResponseHandler.cs
+++ Assets/Scripts/ResponseHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +8,15 @@ public class ResponseHandler : MonoBehaviour
     [SerializeField] private RectTransform responseBox;
     [SerializeField] private RectTransform responseButtonTemplate;
     [SerializeField] private RectTransform responseContainer;
+
+    private DialougeUI dialougeUI;
+
+    List<GameObject> tempDialougeButtons = new List<GameObject>();
+
+    private void Start()
+    {
+        dialougeUI = GetComponent<DialougeUI>();
+    }
     public void ShowResponse(Response[] responses) 
     {
         float responseBoxHeight = 0;
@@ -18,6 +28,7 @@ public class ResponseHandler : MonoBehaviour
             responseButton.GetComponent<TMP_Text>().text= response.ResponseTxt;
             responseButton.GetComponent<Button>().onClick.AddListener(() => onPickedResponse(response));
 
+            tempDialougeButtons.Add(responseButton);
             responseBoxHeight += responseButtonTemplate.sizeDelta.y;
         }
         responseBox.sizeDelta = new Vector2(responseBox.sizeDelta.x,responseBoxHeight);
@@ -25,6 +36,13 @@ public class ResponseHandler : MonoBehaviour
     }
     private void onPickedResponse(Response response) 
     {
-        
+        responseBox.gameObject.SetActive(false);
+        foreach (GameObject button  in tempDialougeButtons) 
+        {
+            Destroy(button);
+        }
+        tempDialougeButtons.Clear();
+
+        dialougeUI.ShowDialoge(response.DialougeObject);
     }
 }
